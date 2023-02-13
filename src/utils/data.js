ab770ddee09cf1c31186d75a7f488b47887c0da9
MassiHadjloum <massihadjloum@gmail.com>
2023-02-13 23:57:19 +0100

    add data list footer

---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+
+export const infoLinksFooter = [
+  {
+    title: "Section 1", list: [
+      { name: "one", to: "" },
+      { name: "two", to:  ""},
+      { name: "three", to: "" },
+      { name: "taf tad", to: "" },
+    ]
+  },
+  {
+    title: "Section 2", list: [
+      { name: "one", to: "" },
+      { name: "two", to:  ""},
+      { name: "three", to: "" },
+      { name: "taf tad", to: "" },
+    ]
+  },
+  {
+    title: "Informations sur FFY", list: [
+      { name: "one", to: "" },
+      { name: "two", to:  ""},
+      { name: "three", to: "" },
+      { name: "taf tad", to: "" },
+    ]
+  },
+  {
+    title: "Contact", list: [
+      { name: "(+33) 6 77 88 99 88", to: "" },
+      { name: "contact@dark-kitchen.fr", to: "" },
+      { name: "1, rue dark kitchen 34544", to: "" },
+
+    ]
+  },
+]
