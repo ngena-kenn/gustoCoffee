a612788cb513c24634a65ec4c4929bfa55871acc
MassiHadjloum <massihadjloum@gmail.com>
2023-02-14 00:04:22 +0100

    footer updated

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,42 +1,42 @@
 import { Stack, Divider, Box } from '@mui/material'
-import { Link } from "react-router-dom"
+import { div } from "react-router-dom"
 import SocialMedia from "./SocialMedia/social_media"
-import style from './style.module.scss'
+import  './footer.scss'
 // import "../../App.scss"
 import { infoLinksFooter } from '../../utils/data'
 
 const Footer = () => {
  
   const infoFooter = infoLinksFooter?.map((box, idx) =>
-    <Box key={idx} className={`${style.grid_item} ${style.midia_text}`}>
-      <div className={style.item_content}>
-        <div className={style.title} >{box.title}</div>
-        <ul className={style.ul}>
+    <Box key={idx} className={'grid_item midia_text'}>
+      <div className={'item_content'}>
+        <div className={'title'} >{box.title}</div>
+        <ul className={'ul'}>
           {box?.list?.map((item, idx) => <li key={item.name}>
-            <Link className={style.midia_text} to={item.to}> {item.name}</Link></li>)}
+            <div className={'midia_text'} to={item.to}> {item.name}</div></li>)}
         </ul>
       </div>
     </Box>
   )
 
   return (
-    <div className={ style.global_footer}>
-      <div className={`regular_text ${style.grid}`}>
-        <div className={style.grille}>
+    <div className={ 'global_footer'}>
+      <div className={'regular_text grid'}>
+        <div className={'grille'}>
           {infoFooter}
         </div>
       </div> 
       <SocialMedia />
-      <Divider className={style.main_divider}></Divider>
+      <Divider className={'main_divider'}></Divider>
       <Stack direction={{ xs: "column", sm: "column", md: "row" }} spacing={{ sm: 1, md: 6 }}
-         className={style.ffooter}>
+         className={'ffooter'}>
         <Box>© Dark-kitchen 2023</Box>
-        <Box><Link to={""}>Conditions générales de vente</Link></Box>
-        <Box><Link to={""}>Confidentialité</Link></Box>
-        <Box><Link to={""}>Mentions légales</Link></Box>
+        <Box><div to={""}>Conditions générales de vente</div></Box>
+        <Box><div to={""}>Confidentialité</div></Box>
+        <Box><div to={""}>Mentions légales</div></Box>
       </Stack>
-      <div style={{ backgroundColor: "white" }} className={style.divider}></div>
-      <div className={style.empty}></div>
+      <div style={{ backgroundColor: "white" }} className={'divider'}></div>
+      <div className={'empty'}></div>
     </div>
   )
 }
