ee609fcf3f9e7feeeda8353f0ff71a17f57ccde7
MassiHadjloum <massihadjloum@gmail.com>
2023-02-14 00:03:31 +0100

    update social component

--- src/components/Footer/SocialMedia/social_media.js
+++ src/components/Footer/SocialMedia/social_media.js
@@ -5,19 +5,19 @@ import  youtube  from "../../../assets/youtube.webp"
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-import style from '../style.module.scss';
+import style from '../footer.scss';
 
 const SocialMedia = () => {
   return (
     <Stack justifyContent={"center"} direction={"row"} spacing={1} alignItems="center" className={style.social}>
       <div>
-        <img width="50" height="50" alt="facebook" className={`${style.social_img} lazyload`} data-src={fb} />
+        <img width="50" height="50" alt="facebook" className={`${style.social_img} lazyload`} data-src={fb} src={fb} />
       </div>
       <div>
-        <img width="50" height="50" alt="instagram" className={`${style.social_img} lazyload`} data-src={insta} />
+        <img width="50" height="50" alt="instagram" className={`${style.social_img} lazyload`} data-src={insta} src={insta} />
       </div>
       <div>
-        <img width="50" height="50" alt="youtube" className={`${style.social_img} lazyload`} data-src={youtube} />
+        <img width="50" height="50" alt="youtube" className={`${style.social_img} lazyload`} data-src={youtube} src={youtube} />
       </div>
     </Stack>
   )
