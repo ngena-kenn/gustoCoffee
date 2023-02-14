4419e9be772660abbfa83800ad813bcd902dd002
kenn_ngena <ngenakenn@yahoo.com>
2023-02-14 11:18:03 +0100

    commit to main

---
+++ src/body/Home.js
@@ -0,0 +1,52 @@
+import React,{Component} from "react";
+import '../css/page.css';
+
+class Home_page extends Component{
+    render(){
+        return(
+            <div>
+            <div class="titre" >notre carte</div>
+              <div class="menu">
+                
+                <div>
+                    <div class="formules aligne-center"></div>
+                    <div class="text" >Formules</div>
+                </div>
+                <div>
+                    <div class="gamme_chaude aligne-center"></div>
+                    <div class="text" >Gammes Chaudes</div>
+                </div>
+                <div>
+                    <div class="poké_creation aligne-center"></div>
+                    <div class="text">Poké Creation</div>
+                </div>
+                <div>
+                    <div class="sides aligne-center"></div>
+                    <div class="text">Sides</div>  
+                </div>
+                <div>
+                    <div class="healthy_bowls aligne-center"></div>
+                    <div class="text">Healthy Bowls</div> 
+                </div>
+                <div>
+                    <div class="desserts aligne-center"></div>
+                    <div class="text">Desserts</div>
+                </div>
+                <div>
+                <div class="boissons_chaudes aligne-center"></div>
+                    <div class="text">Boissons Chaudes</div>
+                </div>
+                <div>
+                    <div class="bubble_tea aligne-center"></div>
+                    <div class="text">Bubble Tea</div> 
+                </div>
+                <div>
+                    <div class="boissons aligne-center"></div>
+                    <div class="text">Boissons</div>  
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+export default Home_page;
