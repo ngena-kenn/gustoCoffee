0bea68210adf003e85f75d62e82c4e0014668bec
MassiHadjloum <massihadjloum@gmail.com>
2023-02-14 10:50:10 +0100

    add main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {Component} from 'react';
-import  Navigation from './body/Navigation';
-im
+import React from 'react';
+import ContainerPage from './components/containerPage/containerPage';
+import Footer from './components/Footer/footer';
+import { desserts } from './data';
+import{ Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-  
-  );
+  <div >
+     <body>
+        
+        <Routes>
+          <Route path="/" element={<div />} />
+          <Route path="/Home" element={<div />} />
+        </Routes>
+    </body>
+  </div>
+  )
 }
 
 export default App;
