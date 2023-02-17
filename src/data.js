cd8531c0b1a0e62fb68c2cfa02682199d46b2d12
MassiHadjloum <massihadjloum@gmail.com>
2023-02-17 15:00:14 +0100

    d

--- src/data.js
+++ src/data.js
@@ -1,128 +1,8 @@
 export const desserts = {
   title: "Dessert",
   data: [
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'MOELLEUX AU CHOCOLAT',
-      description: 'Chocolate lovers… Ce moelleux parsemé de noix de coco râpée est fait pour vous ! Nous avons qu’une seule chose à vous dire : régalez vous bien !',
-      price: '4.90 €',
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'COOKIE BIO: CARAMEL BEURRE SALÉ ',
-      description: 'Les pépites de caramel se mêlent au côté iodé du sel de Guérande… un régal !',
-      price: '2.90 €',
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'COOKIE BIO: CHOCOLAT AU LAIT',
-      description: 'Un cookie à la fois craquant et fondant. Les pépites de chocolat au lait fondent délicieusement en bouche. Une valeur sûre !',
-      price: '2.90 €',
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'CAKE CITRON',
-      description: 'Craquez pour nos super cakes au citron et pavot, fabriqués en France avec des oeufs bio et français, tout comme la farine et le sucre, français eux aussi !',
-      price: '3.90 €',
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'CRUMBLE MYRTILLE CASSIS',
-      description: 'Une compotée de myrtilles et cassis, surmonté d’un délicat crumble bien croustillant ! Produit décongelé, ne pas recongeler.',
-      price: '4.90 €',
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'CAKE MARBRÉ SUPER MOELLEUX',
-      description: 'Craquez pour nos super cakes marbré, fabriqués en France avec des oeufs bio et français, tout comme la farine et le sucre, français eux aussi !',
-      price: '3.90 €',
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'COOKIECOEUR BY LA FABRIQUE À COOKIE',
-      description: 'Dégustez notre cookiecoeur à la praline rose et aux amandes ! Ce cookie vous réchauffera votre cœur et celui de votre partenaire !',
-      price: '2.9 €',
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'CHEESECAKE COULIS EXOTIQUE',
-      description: 'Cheesecake et son coulis frais aux fruits exotiques. Miam Miam, une touche onctueuse et sucrée pour finir en beauté.',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'FRUIT NINJA',
-      description: 'Aussi addictive que le jeu ! Notre salade de fruits exotiques vous apporte une touche de fraicheur avec ses beaux morceaux de mangue fraîche, d’ananas et de grenade.',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'PUDDING VEGAN',
-      description: 'Du lait de coco, des perles de chia et du coulis frais aux fruits exotiques, le dessert parfait pour finir votre escapade hawaïenne ! 252 Kcal',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'FRUIT NINJA',
-      description: 'Aussi addictive que le jeu ! Notre salade de fruits exotiques vous apporte une touche de fraicheur avec ses beaux morceaux de mangue fraîche, d’ananas et de grenade.',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'PUDDING VEGAN',
-      description: 'Du lait de coco, des perles de chia et du coulis frais aux fruits exotiques, le dessert parfait pour finir votre escapade hawaïenne ! 252 Kcal',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'FRUIT NINJA',
-      description: 'Aussi addictive que le jeu ! Notre salade de fruits exotiques vous apporte une touche de fraicheur avec ses beaux morceaux de mangue fraîche, d’ananas et de grenade.',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'PUDDING VEGAN',
-      description: 'Du lait de coco, des perles de chia et du coulis frais aux fruits exotiques, le dessert parfait pour finir votre escapade hawaïenne ! 252 Kcal',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'FRUIT NINJA',
-      description: 'Aussi addictive que le jeu ! Notre salade de fruits exotiques vous apporte une touche de fraicheur avec ses beaux morceaux de mangue fraîche, d’ananas et de grenade.',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'PUDDING VEGAN',
-      description: 'Du lait de coco, des perles de chia et du coulis frais aux fruits exotiques, le dessert parfait pour finir votre escapade hawaïenne ! 252 Kcal',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'FRUIT NINJA',
-      description: 'Aussi addictive que le jeu ! Notre salade de fruits exotiques vous apporte une touche de fraicheur avec ses beaux morceaux de mangue fraîche, d’ananas et de grenade.',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'PUDDING VEGAN',
-      description: 'Du lait de coco, des perles de chia et du coulis frais aux fruits exotiques, le dessert parfait pour finir votre escapade hawaïenne ! 252 Kcal',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'FRUIT NINJA',
-      description: 'Aussi addictive que le jeu ! Notre salade de fruits exotiques vous apporte une touche de fraicheur avec ses beaux morceaux de mangue fraîche, d’ananas et de grenade.',
-      price: '4.90 €'
-    },
-    {
-      picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*',
-      title: 'PUDDING VEGAN',
-      description: 'Du lait de coco, des perles de chia et du coulis frais aux fruits exotiques, le dessert parfait pour finir votre escapade hawaïenne ! 252 Kcal',
-      price: '4.90 €'
-    },
-  ]
-} 
+    { picture: 'https://fac.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Ffac.2F2019.2F12.2F11.2F8e6e6a99-cd00-45a9-b606-01ce2cda0a0e.2Ejpeg/750x562/quality/80/crop-from/center/cr/wqkgSXN0b2NrIC8gRmVtbWUgQWN0dWVsbGU%3D/fondant-au-chocolat-ultra-rapide.jpeg', title: 'MOELLEUX AU CHOCOLAT', description: 'Chocolate lovers… Ce moelleux parsemé de noix de coco râpée est fait pour vous ! Nous avons qu’une seule chose à vous dire : régalez vous bien !', price: '4.90 €', }, { picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdoEzWriNz99yQQ8C_qs7iNYN_eIhBHC-9fA&usqp=CAU', title: 'BOX DE GYOZAS MIXTES', description: 'Les pépites de caramel se mêlent au côté iodé du sel de Guérande… un régal !', price: '2.90 €', }, { picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVNNRL_KsuAZwEIg4pkSRS7lFyUPFCEHy24Q&usqp=CAU', title: 'NUGGETS « MARINS » VÉGÉTAL', description: 'Un cookie à la fois craquant et fondant. Les pépites de chocolat au lait fondent délicieusement en bouche. Une valeur sûre !', price: '2.90 €', }, { picture: 'https://www.undejeunerdesoleil.com/wp-content/uploads/2014/01/Cake_citron.jpg', title: 'CAKE CITRON', description: 'Craquez pour nos super cakes au citron et pavot, fabriqués en France avec des oeufs bio et français, tout comme la farine et le sucre, français eux aussi !', price: '3.90 €', }, { picture: 'https://cookandcrunch.com/wp-content/uploads/2020/07/cookandcrunch-crumbcake-cassis-amande-blogcn-1_1.jpg', title: 'FORMULE BIG SOUPAWA + SIDE', description: 'Une compotée de myrtilles et cassis, surmonté d’un délicat crumble bien croustillant ! Produit décongelé, ne pas recongeler.', price: '4.90 €', }, { picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*', title: 'FORMULE BOWL + SIDE ', description: 'Craquez pour nos super cakes marbré, fabriqués en France avec des oeufs bio et français, tout comme la farine et le sucre, français eux aussi !', price: '3.90 €', }, { picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9sm89uPh8kDXnp1GNBALB0CN0U95mhUQ6sA&usqp=CAU', title: 'FORMULE BIG SOUPAWA + SIDE', description: 'Dégustez notre cookiecoeur à la praline rose et aux amandes ! Ce cookie vous réchauffera votre cœur et celui de votre partenaire !', price: '2.9 €', }, { picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfF80gE1fpzHrWmmzPi30sflx7e8JCywiEsA&usqp=CAU', title: 'FORMULE SMART', description: 'Cheesecake et son coulis frais aux fruits exotiques. Miam Miam, une touche onctueuse et sucrée pour finir en beauté.', price: '4.90 €' }, { picture: 'https://www.conservation-nature.fr/wp-content/uploads/2020/02/salade-de-fruit.jpg', title: 'FRUIT NINJA', description: 'Aussi addictive que le jeu ! Notre salade de fruits exotiques vous apporte une touche de fraicheur avec ses beaux morceaux de mangue fraîche, d’ananas et de grenade.', price: '4.90 €' }, { picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVNNRL_KsuAZwEIg4pkSRS7lFyUPFCEHy24Q&usqp=CAU', title: 'PUDDING VEGAN', description: 'Du lait de coco, des perles de chia et du coulis frais aux fruits exotiques, le dessert parfait pour finir votre escapade hawaïenne ! 252 Kcal', price: '4.90 €' }, { picture: 'https://www.conservation-nature.fr/wp-content/uploads/2020/02/salade-de-fruit.jpg', title: 'FRUIT NINJA', description: 'Aussi addictive que le jeu ! Notre salade de fruits exotiques vous apporte une touche de fraicheur avec ses beaux morceaux de mangue fraîche, d’ananas et de grenade.', price: '4.90 €' }, { picture: 'https://fac.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Ffac.2F2019.2F12.2F11.2F8e6e6a99-cd00-45a9-b606-01ce2cda0a0e.2Ejpeg/750x562/quality/80/crop-from/center/cr/wqkgSXN0b2NrIC8gRmVtbWUgQWN0dWVsbGU%3D/fondant-au-chocolat-ultra-rapide.jpeg', title: 'PUDDING VEGAN', description: 'Du lait de coco, des perles de chia et du coulis frais aux fruits exotiques, le dessert parfait pour finir votre escapade hawaïenne ! 252 Kcal', price: '4.90 €' }, { picture: 'https://www.undejeunerdesoleil.com/wp-content/uploads/2014/01/Cake_citron.jpg', title: 'FRUIT NINJA', description: 'Aussi addictive que le jeu ! Notre salade de fruits exotiques vous apporte une touche de fraicheur avec ses beaux morceaux de mangue fraîche, d’ananas et de grenade.', price: '4.90 €' }, { picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*', title: 'PUDDING VEGAN', description: 'Du lait de coco, des perles de chia et du coulis frais aux fruits exotiques, le dessert parfait pour finir votre escapade hawaïenne ! 252 Kcal', price: '4.90 €' }, { picture: 'https://www.conservation-nature.fr/wp-content/uploads/2020/02/salade-de-fruit.jpg', title: 'FRUIT NINJA', description: 'Aussi addictive que le jeu ! Notre salade de fruits exotiques vous apporte une touche de fraicheur avec ses beaux morceaux de mangue fraîche, d’ananas et de grenade.', price: '4.90 €' }, { picture: 'https://hips.hearstapps.com/hmg-prod/images/chirashi-bowl-1-1656100228.jpg?crop=0.711xw:0.849xh;0,0.0561xh&resize=1200:*', title: 'PUDDING VEGAN', description: 'Du lait de coco, des perles de chia et du coulis frais aux fruits exotiques, le dessert parfait pour finir votre escapade hawaïenne ! 252 Kcal', price: '4.90 €' },]
+}
 export const formule = {
   title: "Formule",
   data: [
@@ -271,7 +151,7 @@ export const formule = {
       price: '4.90 €'
     },
   ]
-} 
+}
 export const gamme_chaude = {
   title: "Gamme Chaude",
   data: [
@@ -432,7 +312,7 @@ export const gamme_chaude = {
       price: '4.90 €'
     },
   ]
-} 
+}
 export const poke_creation = {
   title: "Poke Creation",
   data: [
@@ -593,7 +473,7 @@ export const poke_creation = {
       price: '4.90 €'
     },
   ]
-} 
+}
 export const sides = {
   title: "Sides",
   data: [
@@ -754,7 +634,7 @@ export const sides = {
       price: '4.90 €'
     },
   ]
-} 
+}
 export const healty_bowls = {
   title: "HealtyBowls",
   data: [
@@ -963,7 +843,7 @@ export const healty_bowls = {
       price: '4.90 €'
     },
   ]
-} 
+}
 export const boissons = {
   title: "Boissons",
   data: [
@@ -1148,8 +1028,8 @@ export const boissons = {
       price: '4.90 €'
     },
   ]
-} 
-export const boissons_chaude  = {
+}
+export const boissons_chaude = {
   title: "Boissons Chaude ",
   data: [
     {
@@ -1345,8 +1225,8 @@ export const boissons_chaude  = {
       price: '4.90 €'
     },
   ]
-} 
-export const boissons_tea  = {
+}
+export const boissons_tea = {
   title: "Boissons Tea",
   data: [
     {
